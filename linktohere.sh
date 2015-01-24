87f873ea35ddd7e869e4a102b540873c3e05004f
Kevin Wells <kevin@wellsie.net>
2015-01-24 08:19:52 -0800

    Use hardlinks for files.

--- linktohere.sh
+++ linktohere.sh
@@ -4,4 +4,5 @@ setopt extended_glob
 
 REL_PATH=$(dirname $0)
 
-ln -s $REL_PATH/.*~$REL_PATH/.git~$REL_PATH/.gitmodules .
+ln -s $REL_PATH/.*~$REL_PATH/.git(/) .
+ln $REL_PATH/.*~$REL_PATH/.gitmodules(.) .
